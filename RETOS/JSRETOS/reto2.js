651f5a91509684ca3af40868854c84aeaaaf16e7
Caro.ihp <carolina.hp251014@gmail.com>
2023-09-16 00:09:56 -0300

    Actualizacion3 reto2

--- RETOS/JSRETOS/reto2.js
+++ RETOS/JSRETOS/reto2.js
@@ -1,19 +1,22 @@
 //Nota 1
 let nota1 = parseFloat(prompt("Ingrese la nota 1: ", ''));
-  if (isNaN(nota1) || nota1 < 1 || nota1 > 7) {
+  while (isNaN(nota1) || nota1 < 1 || nota1 > 7) {
     alert("La nota 1 no es valida. Debe ser un número entre 1 y 7");
+    nota1 = parseFloat(prompt("Ingrese la nota 1: ", ''));
   }
 
 //Nota 2
 let nota2 = parseFloat(prompt("Ingrese la nota 2: ", ''));
-  if (isNaN(nota2) || nota2 < 1 || nota2 > 7) {
+  while (isNaN(nota2) || nota2 < 1 || nota2 > 7) {
     alert("La nota 1 no es valida. Debe ser un número entre 1 y 7");
+    nota2 = parseFloat(prompt("Ingrese la nota 2: ", ''));
   }
 
 //Nota 3
 let nota3 = parseFloat(prompt("Ingrese la nota 3: ", ''));
-  if (isNaN(nota3) || nota3 < 1 || nota3 > 7) {
+  while (isNaN(nota3) || nota3 < 1 || nota3 > 7) {
     alert("La nota 1 no es valida. Debe ser un número entre 1 y 7");
+    nota3 = parseFloat(prompt("Ingrese la nota 3: ", ''));
   }
   
 //Promedio
@@ -21,9 +24,9 @@ const promedio = (nota1 * 0.4 + nota2 * 0.3 + nota3 * 0.3).toFixed(1);
 
 //Validar Promedio
   if (promedio < 3.95) {
-    alert(`Has reprobado la asignatura. Promedio: ${promedio}`);
+    alert(`Has reprobado la asignatura. Con Promedio: ${promedio}`);
   } else if (promedio >= 3.95 && promedio <= 4.94) {
-    alert(`Vas a examen. Promedio: ${promedio}`);
+    alert(`Vas a examen. Con Promedio: ${promedio}`);
   } else {
-    alert(`Te eximes de la asignatura. Promedio: ${promedio}`);
+    alert(`Te eximes de la asignatura. Con Promedio: ${promedio}`);
   }
