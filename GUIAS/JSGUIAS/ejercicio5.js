04ab5086322ab651c87ad6101c6f773193149edf
Caro.ihp <carolina.hp251014@gmail.com>
2023-09-27 17:55:09 -0300

    Actualizacion Guia1

--- GUIAS/JSGUIAS/ejercicio5.js
+++
@@ -1,3 +0,0 @@
-//Realizar un programa que permita al usuario ingresar su estatura y peso, para que se
-//muestre en el navegador su IMC correspondiente. Además de mostrar el IMC, indicar si está
-//en la categoría de: “Bajo Peso”, “Peso Normal” o “Sobrepeso”.
